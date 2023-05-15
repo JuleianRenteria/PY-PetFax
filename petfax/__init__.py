b074eff41b5ca664e07670d7699746dfa345bcc3
Juleian Renteria <jprenteria02@gmail.com>
2023-05-15 15:09:52 -0700

    stlying

---
+++ petfax/__init__.py
@@ -0,0 +1,34 @@
+# config                    
+from flask import Flask
+from flask_migrate import Migrate
+
+# factory
+def create_app():
+    app = Flask(__name__)
+    
+    # database config
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:2J1bzy9489@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False     
+
+    from . import models 
+    models.db.init_app(app) 
+    migrate = Migrate(app, models.db)
+
+    # index route
+    @app.route('/')
+    def index(): 
+        return 'Hello, this is PetFax!'
+
+    # register pet blueprint 
+    from . import pet 
+    app.register_blueprint(pet.bp)
+
+    # register fact blueprint 
+    from . import fact
+    app.register_blueprint(fact.bp)
+
+    # return the app 
+    return app
+
+
+ 
